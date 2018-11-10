7c40f6b71f8fd3e825d65e2f8d5c03501a4bfd4b
Naeem Khan <naeemukhan14@gmail.com>
2018-11-10 13:56:27 +0400

    Bug fix: Since it's a single direction graph, flights FROM and TO the same cities had different costs, while they should be the same.

--- src/F28DA_CW2/DirectFlightsAndLeastCost.java
+++ src/F28DA_CW2/DirectFlightsAndLeastCost.java
@@ -7,6 +7,15 @@ import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleDirectedWeightedGraph;
 
+/**
+ * @author Naeem Khan
+ *	
+ *	This class is made to create and work with SimpleDirectedWeightedGraph,
+ *	for part A of Sky Routes. This will help avoid cluster in main class, 
+ *	and follow the concepts of OOP.
+ */
+
+
 public class DirectFlightsAndLeastCost {
 	
 	private SimpleDirectedWeightedGraph<String, DefaultWeightedEdge> simpleGraph;
@@ -23,13 +32,19 @@ public class DirectFlightsAndLeastCost {
 		simpleGraph.addVertex("Kuala Lumpur");
 	}
 	
-	public void addGraphEdge() {
+	public void addGraphEdges() {
 		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Edinburgh", "Heathrow"), 80);
+		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Heathrow" ,"Edinburgh"), 80);
 		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Heathrow", "Dubai"), 130);
+		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Dubai", "Heathrow"), 130);
 		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Heathrow", "Sydney"), 570);
+		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Sydney", "Heathrow"), 570);
 		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Dubai", "Kuala Lumpur"), 170);
+		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Kuala Lumpur", "Dubai"), 170);
 		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Dubai", "Edinburgh"), 190);
+		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Edinburgh", "Dubai"), 190);
 		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Kuala Lumpur", "Sydney"), 150);
+		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Sydney", "Kuala Lumpur"), 150);
 	}
 	
 	public void getShortestPath(String from, String to) {
