23d9ae776a913bebfc69db88bb75396678429536
Naeem Khan <naeemukhan14@gmail.com>
2018-11-11 01:45:13 +0400

    Successfully tested required code for partB() method in SkyRoutes class

--- src/F28DA_CW2/hello.java
+++ src/F28DA_CW2/hello.java
@@ -1,48 +1,39 @@
 package F28DA_CW2;
 
 import java.io.FileNotFoundException;
-import java.util.HashSet;
+import java.util.HashMap;
+
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.SimpleDirectedWeightedGraph;
 
 public class hello {
-	public static void a() throws FileNotFoundException, SkyRoutesException {
-		FlightsReader f = new FlightsReader(FlightsReader.MOREAIRLINECODES);
-		
-		HashSet<String[]> airlines = f.getAirlines();
-		String result = "";
-		
-		for(String[] s : airlines) {
-			for(int i = 0; i < s.length; i++) {
-				result += s[i] + " ";
-			}
-			result += "\n";
-		}
-		
-		//System.out.println(result);
-		
-		HashSet<String[]> airports = f.getAirports();
-		String resultAirports = "";
-		
-		for(String[] s : airports) {
-			for(int i = 0; i < s.length; i++) {
-				resultAirports += s[i] + " ";
-			}
-			resultAirports += "\n";
-		}
-		//System.out.println(resultAirports);
-		
-		HashSet<String[]> flights = f.getFlights();
-		String resultFlights = "";
-		
-		for(String[] s : flights) {
-			for(int i = 0; i < s.length; i++) {
-				resultFlights += s[i] + " ";
-			}
-			resultFlights += "\n";
-		}
-		System.out.println(resultFlights);
-		
-	}
+	
 	public static void main(String[] args) throws FileNotFoundException, SkyRoutesException {
-		a();
+		
+		SimpleDirectedWeightedGraph<String, FlightsInfo> m = new SimpleDirectedWeightedGraph<String, FlightsInfo>(FlightsInfo.class);
+		
+		FlightsReader fr = new FlightsReader(FlightsReader.MOREAIRLINECODES);
+		HashMap<String, String> airLines = new HashMap<String, String>();
+		
+		for(String[] s : fr.getAirports()) {
+			for(int i = 0; i < s.length; i+=3) {
+				airLines.put(s[0], s[1]);
+				m.addVertex(s[i]);
+			}
+		}
+		
+		for(String[] s : fr.getFlights()) {
+			for(int i = 0; i < s.length; i++) {
+				FlightsInfo f = new FlightsInfo(s[0], airLines.get(s[1]), s[2], airLines.get(s[3]), s[4]);
+				m.addEdge(s[1], s[3], f);
+				m.setEdgeWeight(f, (int) Double.parseDouble(s[5]));
+			}
+		}
+
+		GraphPath<String, FlightsInfo> shortestPath = DijkstraShortestPath.findPathBetween(m, "EDI", "SYD");
+		System.out.println(shortestPath);
+		System.out.println(shortestPath.getWeight());
+		System.out.println(airLines.get("Edinburgh"));
 	}
 }
