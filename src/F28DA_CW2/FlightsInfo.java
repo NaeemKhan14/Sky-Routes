25cb62ec8f936180a4183a4da73e1f3a54190b40
Naeem Khan <naeemukhan14@gmail.com>
2018-11-12 05:05:34 +0400

    Documented and changed the way output was printing.

--- src/F28DA_CW2/FlightsInfo.java
+++ src/F28DA_CW2/FlightsInfo.java
@@ -7,7 +7,8 @@ import org.jgrapht.graph.DefaultWeightedEdge;
 
 /**
  * @author Naeem Khan
- *
+ *	This class stores the individual flight information in the edge
+ *	of the graph
  */
 public class FlightsInfo extends DefaultWeightedEdge {
 	/**
@@ -15,8 +16,10 @@ public class FlightsInfo extends DefaultWeightedEdge {
 	 */
 	private static final long serialVersionUID = 5801941127947694079L;
 	
+	// Fields
 	private final String flightCode, arrivalTime, departureTime, arrivalAirport, departureAirport;
 	
+	// Constructor
 	public FlightsInfo(String flightCode, String departureAirport, String departureTime, String arrivalAirport,
 			String arrivalTime) {
 		this.flightCode = flightCode;
@@ -26,9 +29,14 @@ public class FlightsInfo extends DefaultWeightedEdge {
 		this.arrivalTime = arrivalTime;
 	}
 	
+	/**
+	 * Overriding the default toString() method of DefaultWeightedEdge
+	 * class to display the output that we require
+	 */
+	
 	@Override
 	public String toString() {
-		return "(" + departureAirport + " : " + departureTime + " : " + flightCode + " : " + arrivalAirport + " : " + arrivalTime + ")";
+		return departureAirport + ":" + departureTime + ":" + flightCode + ":" + arrivalAirport + ":" + arrivalTime;
 	}
 
 }
