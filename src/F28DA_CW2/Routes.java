93ae22b756bd25dd46304b12136110fd387bcb73
Naeem Khan <naeemukhan14@gmail.com>
2018-11-23 21:27:12 +0400

    End of Course Work 2.

--- src/F28DA_CW2/Routes.java
+++ src/F28DA_CW2/Routes.java
@@ -3,12 +3,14 @@ package F28DA_CW2;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
+
 import static java.time.temporal.ChronoUnit.MINUTES;
 import java.util.List;
 
+import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.graph.SimpleDirectedWeightedGraph;
 
 /**
  * @author Naeem Khan
@@ -21,7 +23,7 @@ public class Routes implements IRoute {
 	
 	private GraphPath<String, FlightsInfo> shortestPath;
 	private DateTimeFormatter dateFormat;
-	
+	private HashMap<String, String> flightsPrice;
 	/**
 	 * Constructor of this class which finds the shortest path for our
 	 * journey
@@ -31,12 +33,24 @@ public class Routes implements IRoute {
 	 * @param to	Destination airport
 	 */
 	
-	public Routes(SimpleDirectedWeightedGraph<String, FlightsInfo> graph, String from, String to) {
-		DijkstraShortestPath<String, FlightsInfo> shortestPathGraph = new DijkstraShortestPath<String, FlightsInfo>(graph);
+	public Routes(Graph<String, FlightsInfo> graph, String from, String to) {
+		DijkstraShortestPath<String, FlightsInfo> shortestPathGraph
+		= new DijkstraShortestPath<String, FlightsInfo>(graph);
 		shortestPath = shortestPathGraph.getPath(from, to);
 		dateFormat = DateTimeFormatter.ofPattern("HHmm");	
 	}
 
+	// Second constructor for least hops
+	public Routes(Graph<String, FlightsInfo> graph, String from, String to, 
+			HashMap<String, String> flightsPrice) {
+		DijkstraShortestPath<String, FlightsInfo> shortestPathGraph
+		= new DijkstraShortestPath<String, FlightsInfo>(graph);
+		shortestPath = shortestPathGraph.getPath(from, to);
+		dateFormat = DateTimeFormatter.ofPattern("HHmm");
+		this.flightsPrice = flightsPrice;
+	}
+	
+	
 	/** Returns the list of airports codes composing the route */
 	
 	@Override
@@ -44,37 +58,63 @@ public class Routes implements IRoute {
 		
 		List<String> airportsList = new ArrayList<String>();
 		
-		for(String[] data : getEdgeData()) {
-			if(!airportsList.contains(data[0])) {
-				airportsList.add(data[0]);
+		try {
+			for(String[] data : getEdgeData()) {
+				if(!airportsList.contains(data[0])) {
+					airportsList.add(data[0]);
+				}
+				if(!airportsList.contains(data[3])) {
+					airportsList.add(data[3]);
+				}
 			}
-			if(!airportsList.contains(data[3])) {
-				airportsList.add(data[3]);
-			}
+		} catch (SkyRoutesException e) {
+			e.printStackTrace();
 		}
 		
 		return airportsList;
 	}
 	
-	/** Returns the list of airports codes composing the route */
+	/** Returns the list of flight codes composing the route */
 	
 	@Override
 	public List<String> getFlights() {
 		List<String> flightsList = new ArrayList<String>();
 		
-		for(String[] data : getEdgeData()) {
-			flightsList.add(data[2]);
+		try {
+			for(String[] data : getEdgeData()) {
+				flightsList.add(data[2]);
+			}
+		} catch (SkyRoutesException e) {
+			e.printStackTrace();
 		}
-		
+
 		return flightsList;
+	}
+
+	@Override
+	public int totalHop() {
+		return shortestPath.getLength();
 	}
 	
 	@Override
-	public int totalHop() {
-		return getEdgeData().size();
-	}
-	@Override
 	public int totalCost() {
+		
+		/*
+		 * In case of least hops, the graph used is an unweighted graph, it
+		 * does not contain any edge weights. Since every flight code is unique
+		 * in the flights database, I stored each flight's price in a hashmap,
+		 * and used it here to get that flight's price.
+		 */
+		
+		if((int) shortestPath.getWeight() == totalHop()) {
+			int price = 0;
+			
+			for(String flight : getFlights()) {
+				price += Integer.valueOf(flightsPrice.get(flight));
+			}
+			return price;
+		}
+		
 		return (int) shortestPath.getWeight();
 	}
 	
@@ -93,16 +133,20 @@ public class Routes implements IRoute {
 		
 		int result = 0;
 		
-		for(String[] flightData : getEdgeData()) {
-			LocalTime departureTime = LocalTime.parse(flightData[1], dateFormat);
-			LocalTime arrivalTime = LocalTime.parse(flightData[4], dateFormat);		
-			/*
-			 * If the value we get from comparing the difference between two times is
-			 * below 0 (i.e. difference between 1400 -> 0900 gives us a negative because 
-			 * LocalTime() cannot differentiate between day changes). In which case we add
-			 * a whole day to our result to get the correct value. The code below does that.
-			 */			
-			result += (MINUTES.between(departureTime, arrivalTime) + 1440) % 1440;
+		try {
+			for(String[] flightData : getEdgeData()) {
+				LocalTime departureTime = LocalTime.parse(flightData[1], dateFormat);
+				LocalTime arrivalTime = LocalTime.parse(flightData[4], dateFormat);		
+				/*
+				 * If the value we get from comparing the difference between two times is
+				 * below 0 (i.e. difference between 1400 -> 0900 gives us a negative because 
+				 * LocalTime() cannot differentiate between day changes). In which case we add
+				 * a whole day to our result to get the correct value. The code below does that.
+				 */			
+				result += (MINUTES.between(departureTime, arrivalTime) + 1440) % 1440;
+			}
+		} catch (SkyRoutesException e) {
+			e.printStackTrace();
 		}
 		
 		return result;
@@ -114,11 +158,15 @@ public class Routes implements IRoute {
 	public int connectingTime() {
 		int result = 0;
 		
-		for(int i = 0; i < getEdgeData().size()-1; i++) {
-			LocalTime arrivalTime = LocalTime.parse(getEdgeData().get(i)[4], dateFormat);
-			LocalTime departureTime = LocalTime.parse(getEdgeData().get(i+1)[1], dateFormat);
-			
-			result += (MINUTES.between(arrivalTime, departureTime) + 1440) % 1440;
+		try {
+			for(int i = 0; i < getEdgeData().size()-1; i++) {
+				LocalTime arrivalTime = LocalTime.parse(getEdgeData().get(i)[4], dateFormat);
+				LocalTime departureTime = LocalTime.parse(getEdgeData().get(i+1)[1], dateFormat);
+				
+				result += (MINUTES.between(arrivalTime, departureTime) + 1440) % 1440;
+			}
+		} catch (SkyRoutesException e) {
+			e.printStackTrace();
 		}
 	
 		return result;
@@ -138,10 +186,15 @@ public class Routes implements IRoute {
 	 * towards the target
 	 * 
 	 * @return List of all the data of each connection flight (i.e. graph edge)
+	 * @exception If no connections were found between two vertices
 	 */
-	
-	@Override
-	public List<String[]> getEdgeData() {
+
+
+	public List<String[]> getEdgeData() throws SkyRoutesException {
+		
+		if(shortestPath == null) {
+			throw new SkyRoutesException("No flights were found for this route.");
+		}
 		
 		List<String[]> edgeData = new ArrayList<String[]>();
 		
--- src/F28DA_CW2/SkyRoutes.java
+++ src/F28DA_CW2/SkyRoutes.java
@@ -7,18 +7,23 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
 
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.AsUnweightedGraph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.DirectedWeightedMultigraph;
 import org.jgrapht.graph.SimpleDirectedWeightedGraph;
 
 
 public class SkyRoutes implements IRoutes {
 
-	private static DirectFlightsAndLeastCost directCost;
 	private static FlightsReader flightsReader;
-	private static SimpleDirectedWeightedGraph<String, FlightsInfo> graph;
-	private static HashMap<String, String> airportNames;
+	private static DirectedWeightedMultigraph<String, FlightsInfo> graph;
+	private static HashMap<String, String> airportNames, flightsCost;
+	private static Routes flightsList;
 	
 	public SkyRoutes() {
-		graph = new SimpleDirectedWeightedGraph<String, FlightsInfo>(FlightsInfo.class);
+		graph = new DirectedWeightedMultigraph<String, FlightsInfo>(FlightsInfo.class);
 		airportNames = new HashMap<String, String>();
 		try {
 			flightsReader = new FlightsReader(FlightsReader.MOREAIRLINECODES);
@@ -29,25 +34,52 @@ public class SkyRoutes implements IRoutes {
 	
 	public static void partA() {
 		
-		directCost = new DirectFlightsAndLeastCost();
-		directCost.addGraphVertex();
-		directCost.addGraphEdges();
+		SimpleDirectedWeightedGraph<String, DefaultWeightedEdge> simpleGraph = 
+				new SimpleDirectedWeightedGraph<String, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+		
+		simpleGraph.addVertex("Dubai");
+		simpleGraph.addVertex("Edinburgh");
+		simpleGraph.addVertex("Heathrow");
+		simpleGraph.addVertex("Sydney");
+		simpleGraph.addVertex("Kuala Lumpur");
+		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Edinburgh", "Heathrow"), 80);
+		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Heathrow" ,"Edinburgh"), 80);
+		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Heathrow", "Dubai"), 130);
+		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Dubai", "Heathrow"), 130);
+		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Heathrow", "Sydney"), 570);
+		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Sydney", "Heathrow"), 570);
+		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Dubai", "Kuala Lumpur"), 170);
+		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Kuala Lumpur", "Dubai"), 170);
+		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Dubai", "Edinburgh"), 190);
+		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Edinburgh", "Dubai"), 190);
+		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Kuala Lumpur", "Sydney"), 150);
+		simpleGraph.setEdgeWeight(simpleGraph.addEdge("Sydney", "Kuala Lumpur"), 150);
+		
 		
 		System.out.println("The following airports are used:");
 		
-		for(String s : directCost.getVertexes()) {
+		for(String s : simpleGraph.vertexSet()) {
 			System.out.println("      " + s);
 		}
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Please enter the start airport");
-		String input = scan.next();
+		String from = scan.nextLine().trim();
 		
 		System.out.println("Please enter the destination airport");
-		String input2 = scan.next();
+		String to = scan.nextLine().trim();
 		scan.close();
 		
-		directCost.getShortestPath(input, input2);
+		DijkstraShortestPath<String, DefaultWeightedEdge> d = new DijkstraShortestPath<>(simpleGraph);
+		GraphPath<String, DefaultWeightedEdge> shortestPath = d.getPath(from, to);
+		
+		System.out.println("Shortest (i.e. cheapest) path:");
+		
+		for(int i = 0; i < shortestPath.getVertexList().size()-1; i++) {
+			System.out.println(i+1 + ". " + shortestPath.getVertexList().get(i) + " -> " + shortestPath.getVertexList().get(i+1));
+		}
+		
+		System.out.println("Cost of shortest (i.e. cheapest) path = £" + (int) shortestPath.getWeight());
 		
 	}
 
@@ -67,13 +99,14 @@ public class SkyRoutes implements IRoutes {
 		System.out.println("Route for " + airportNames.get(start) + " to " + airportNames.get(end));
 		try {
 			IRoute travelInfo = skyRoutes.leastCost(start, end);
-			System.out.println(String.format("%1$-5s %2$-15s %3$-5s %4$-10s %5$-15s %6$s", "leg", "leave", "At", "On", "Arrive", "At"));
+			System.out.println(String.format("%1$-5s %2$-15s %3$-5s %4$-10s %5$-15s %6$s",
+					"leg", "leave", "At", "On", "Arrive", "At"));
 			
 			int count = 1;
 			
-			for(String[] result : travelInfo.getEdgeData()) {
+			for(String[] result : flightsList.getEdgeData()) {
 				System.out.println(String.format("%1$-5d %2$-15s %3$-5s %4$-10s %5$-15s %6$s", count++, 
-						airportNames.get(result[0]), result[1], result[2], airportNames.get(result[3]), result[4]));
+						result[0], result[1], result[2], result[3], result[4]));
 			}
 			System.out.println("Total Journey Cost = £" + travelInfo.totalCost());
 			System.out.println("Total travel time = " + travelInfo.totalTime() + " minutes");
@@ -98,22 +131,26 @@ public class SkyRoutes implements IRoutes {
 		System.out.println("Route for " + airportNames.get(start) + " to " + airportNames.get(end));
 		try {
 			List<String> exclusionList = new ArrayList<String>();
-			exclusionList.add("LHR");
-			exclusionList.add("BKK");
-			IRoute travelInfo = skyRoutes.leastCost(start, end, exclusionList);
-			System.out.println(String.format("%1$-5s %2$-15s %3$-5s %4$-10s %5$-15s %6$s", "leg", "leave", "At", "On", "Arrive", "At"));
+			exclusionList.add("NTQ");
+			exclusionList.add("LPQ");
+			
+			IRoute travelInfo = skyRoutes.leastHop(start, end);
+			System.out.println(String.format("%1$-5s %2$-15s %3$-5s %4$-10s %5$-15s %6$s",
+					"leg", "leave", "At", "On", "Arrive", "At"));
 			
 			int count = 1;
 			
-			for(String[] result : travelInfo.getEdgeData()) {
+			for(String[] result : flightsList.getEdgeData()) {
 				System.out.println(String.format("%1$-5d %2$-15s %3$-5s %4$-10s %5$-15s %6$s", count++, 
-						result[0], result[1], result[2], result[3], result[4]));
+						airportNames.get(result[0]), result[1], result[2], airportNames.get(result[3]), result[4]));
 			}
+			
 			System.out.println("Total Journey Cost = £" + travelInfo.totalCost());
 			System.out.println("Total Number of Connections: " + travelInfo.totalHop());
 			System.out.println("Total Time in Air: " + travelInfo.airTime() + " Minutes");
 			System.out.println("Total Time Spent Waiting: " + travelInfo.connectingTime() + " Minutes");
 			System.out.println("Total travel time = " + travelInfo.totalTime() + " Minutes");
+
 		} catch (SkyRoutesException e) {
 			e.printStackTrace();
 		}
@@ -122,6 +159,8 @@ public class SkyRoutes implements IRoutes {
 
 	public static void main(String[] args) {
 		partC();
+//		partB();
+//		partC();
 		
 	}
 
@@ -140,7 +179,10 @@ public class SkyRoutes implements IRoutes {
 		// Get the airports data and all the airport codes as vertices
 		for(String[] airportsList : airports) {
 			airportNames.put(airportsList[0], airportsList[1]); // Put airport codes as keys, and airport names as their values
-			graph.addVertex(airportsList[0]); // Add the airport names as vertices
+			// 
+			if(!graph.addVertex(airportsList[0])) { 
+				return false;
+			}
 		}
 		
 		/*
@@ -149,38 +191,89 @@ public class SkyRoutes implements IRoutes {
 		 */
 		
 		for(String[] flightsInfo : routes) {
-			graph.addEdge(flightsInfo[1], flightsInfo[3], new FlightsInfo(flightsInfo[0], flightsInfo[1], flightsInfo[2], flightsInfo[3], flightsInfo[4]));
-			graph.setEdgeWeight(graph.getEdge(flightsInfo[1], flightsInfo[3]), Integer.parseInt(flightsInfo[5]));
+			FlightsInfo edgeInfo = new FlightsInfo(flightsInfo[0], flightsInfo[1], flightsInfo[2], 
+					flightsInfo[3], flightsInfo[4]);
+			if(!graph.addEdge(flightsInfo[1], flightsInfo[3], edgeInfo)) {
+				return false;
+			}
+			graph.setEdgeWeight(edgeInfo, Integer.parseInt(flightsInfo[5]));
 		}
 		
-		// FIX THIS!!!!!!!
 		return true;
 	}
 
 	@Override
 	public IRoute leastCost(String from, String to) throws SkyRoutesException {
-		return new Routes(graph, from, to);
+		// If the airports we are looking for do not exist in our graph,
+		// it will throw an error
+		if(!graph.containsVertex(from) || !graph.containsVertex(to)) {
+			throw new SkyRoutesException("Target airport does not exist");
+		}
+		
+		return flightsList = new Routes(graph, from, to);
 	}
 
 	@Override
 	public IRoute leastHop(String from, String to) throws SkyRoutesException {
-		return null;
+		
+		if(!graph.containsVertex(from) || !graph.containsVertex(to)) {
+			throw new SkyRoutesException("Target airport does not exist");
+		}
+		/*
+		 * In case of least hops, the graph used is an unweighted graph, it
+		 * does not contain any edge weights. Since every flight code is unique
+		 * in the flights database, I stored each flight's price in a hashmap,
+		 * and used it in Routes class to get that flight's price.
+		 */
+		flightsCost = new HashMap<>();
+		
+		for(String[] flightsInfo : flightsReader.getFlights()) {
+			flightsCost.put(flightsInfo[0], flightsInfo[5]);
+		}
+		
+		AsUnweightedGraph<String, FlightsInfo> unweightedGraph = new AsUnweightedGraph<>(graph);
+		return flightsList = new Routes(unweightedGraph, from, to, flightsCost);
 	}
 
 	@Override
 	public IRoute leastCost(String from, String to, List<String> excluding) throws SkyRoutesException {
 		
+		if(!graph.containsVertex(from) || !graph.containsVertex(to)) {
+			throw new SkyRoutesException("Target airport does not exist");
+		}
+		
+		/* 
+		 *	Remove all the edges of the given airport so they do not appear
+		 *	in our search
+		*/
 		for(String airportCodes: excluding) {
 			graph.removeAllEdges(graph.edgesOf(airportCodes));
 		}
 		
-		return new Routes(graph, from, to);
+		return flightsList = new Routes(graph, from, to);
 	}
 
 	@Override
 	public IRoute leastHop(String from, String to, List<String> excluding) throws SkyRoutesException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		if(!graph.containsVertex(from) || !graph.containsVertex(to)) {
+			throw new SkyRoutesException("Target airport does not exist");
+		}
+		
+		flightsCost = new HashMap<>();
+		
+		for(String[] flightsInfo : flightsReader.getFlights()) {
+			flightsCost.put(flightsInfo[0], flightsInfo[5]);
+		}
+		
+		for(String airportCodes: excluding) {
+			graph.removeAllEdges(graph.edgesOf(airportCodes));
+		}
+		
+
+		AsUnweightedGraph<String, FlightsInfo> unweightedGraph = new AsUnweightedGraph<>(graph);
+		
+		return flightsList = new Routes(unweightedGraph, from, to, flightsCost);
 	}
 
 	@Override
